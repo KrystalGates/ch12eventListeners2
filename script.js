832ee3013a0bbc3db77d3d3528e2204368abbb56
krystal gates <krystal_sabrina@yahoo.com>
2019-06-14 15:29:13 -0500

    initial HTML and instruction for exercise

---
+++ script.js
@@ -0,0 +1,5 @@
+// Challenge: Dynamic Cards
+// Create an HTML page that contains a text area and a button labeled Create.
+// When the user enters in text into the text area and then clicks the create button, use a factory function that creates a new DOM component that has a border, and includes it's own delete button.
+// Insert that new component into the DOM.
+// When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
